db823e453e1cb03dac6a009801975aa0e3b27dcd
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-06-06 23:11:24 +0530

    user navbar edited

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Admin Centre */
 router.get('/', function(req, res, next) {
  res.render("index")
 });
