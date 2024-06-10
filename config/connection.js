ebf6dfd35fbae0b17d2446e2f2f4e8cfba2a74c7
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-06-11 04:56:21 +0530

    DB connected

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,39 @@
-var MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const state={
     db:null
 }
 
 module.exports.connect=function(done){ //you can use the function connect by using requiring connection.js
-    const url = 'mongodb://127.0.0.1:27017';
+    const url = 'mongodb://0.0.0.0:27017/'
     const dbname='shopping'
 
-    MongoClient.connect(url,(err,data)=>{
-        if(err) return done(err)
-        else state.db=data.db(dbname)
-        done()
-        console.log("Connected")
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);
 
-    })
+async function run() {
+try {
+// Connect the client to the server (optional starting in v4.7)
+await client.connect();
+
+// Send a ping to confirm a successful connection
+await client.db("admin").command({ ping: 1 });
+state.db=client.db(dbname)
+    done()
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+// Ensures that the client will close when you finish/error
+await client.close();
+}
+}
+run().catch(console.dir);
+
 }
 
 module.exports.get=function(){
