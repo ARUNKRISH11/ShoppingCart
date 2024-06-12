d1d05a3e25a91c5f0e581b5d6e717d7a46ff29fc
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-06-12 20:21:16 +0530

    add product admin table view

---
+++ config/collection.js
@@ -0,0 +1,4 @@
+module.exports={
+    //write in capital, because its constant
+    PRODUCT_COLLECTION:'product'
+}
--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,11 +1,15 @@
 //Product related functions
 
-//var db=require('../config/connection').get
+//var db=require('../config/connection')
+var collection = require('../config/collection')
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = 'mongodb://0.0.0.0:27017/'
+//dbName and collectionName
+const dbName = 'shopping'
+const collectionName = 'product'
 
-const client = new MongoClient(uri,  {
+const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
@@ -20,16 +24,24 @@ module.exports = {
         console.log(productDetailes)
 
         async function productDB(product) {
-            //dbName and collectionName
-            const dbName = 'shopping'
-            const collectionName = 'product'
-            await client.db(dbName).collection(collectionName).insertOne(product).then((data)=>{
+            await client.db(dbName).collection(collectionName).insertOne(product).then((data) => {
                 //accessing Object id and return
-                const objectId =(data.insertedId).toString()
-                console.log("Id from product helpers",objectId)
+                const objectId = (data.insertedId).toString()
+                console.log("Id from product helpers", objectId)
                 callback(objectId)
             })
         }
         productDB(productDetailes)
+    },
+    //accessing product detailes from DB
+    //Promise: recive the data using 'then', where the function is calling
+    getAllProducts: () => {
+        return new Promise(async (resolve, reject) => {
+            let products = await client.db(dbName).collection(collectionName).find().toArray()
+            //let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
     }
+
+
 }
