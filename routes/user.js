eb727ffa7cb8a051c3ff984bf9abd22fcb678f67
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-06-15 15:39:31 +0530

    user logged in back page reload

--- routes/user.js
+++ routes/user.js
@@ -7,23 +7,27 @@ const userHelpers = require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function (req, res, next) {
   //user loggedin section
-  let user=req.session.user
+  let user = req.session.user
   //accessing product detailes from DB
   productHelpers.getAllProducts().then((products) => {
-    console.log('session',user)
+    //console.log('session', user)
     res.render("user/view-products", { products, admin: false, user })
   })
 });
 router.get('/login', (req, res, next) => {
-  res.render("user/login")
+  if (req.session.loggedIn) {
+    res.redirect('/')
+  } else {
+    res.render("user/login")
+  }
 })
 router.post('/user/login', (req, res, next) => {
   userHelpers.doLogin(req.body).then((response) => {
     //console.log(response.staus)
     if (response.status) {
       //user loggedin session
-      req.session.loggedIn=true
-      req.session.user=response.user
+      req.session.loggedIn = true
+      req.session.user = response.user
       //the page already mentioned above. So there using redirect
       res.redirect('/')
     } else {
@@ -40,7 +44,7 @@ router.post('/user/signup', (req, res, next) => {
     res.redirect('/login')
   })
 })
-router.get('/logout', (req, res, next)=>{
+router.get('/logout', (req, res, next) => {
   //clearing the session
   req.session.destroy()
   res.render('user/logout')
